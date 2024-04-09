664cdd637bcb6d2f149f339b075ae4751e139599
Mohammmad Qebalah <ma3571318z@gmail.com>
2024-04-09 04:00:52 -0700

    Update private-service.dto.ts

--- libs/api/modules/admin/src/lib/resources/private-server/private-service.dto.ts
+++ libs/api/modules/admin/src/lib/resources/private-server/private-service.dto.ts
@@ -1,12 +1,19 @@
 import { ApiProperty } from '@nestjs/swagger';
+import { IsNotEmpty, IsString } from 'class-validator';
 
 export class CreatePrivateServiceDto {
   @ApiProperty()
+  @IsString()
+  @IsNotEmpty()
   name_ar!: string;
 
   @ApiProperty()
+  @IsString()
+  @IsNotEmpty()
   name_en!: string;
 
   @ApiProperty()
+  @IsString()
+  @IsNotEmpty()
   name_he!: string;
 }
