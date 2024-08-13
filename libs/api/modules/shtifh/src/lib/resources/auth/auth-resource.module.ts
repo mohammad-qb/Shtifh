d7b5ec25a1dc6dfc0f3938c1138afc589eabb3ed
Mohammmad Qebalah <ma3571318z@gmail.com>
2024-08-13 10:16:28 -0700

    Update auth-resource.module.ts

--- libs/api/modules/shtifh/src/lib/resources/auth/auth-resource.module.ts
+++ libs/api/modules/shtifh/src/lib/resources/auth/auth-resource.module.ts
@@ -3,9 +3,10 @@ import { PrismaModule } from '@shtifh/prisma-service';
 import { AuthResourceService } from './auth-resource.service';
 import { AuthResourceController } from './auth-resource.controller';
 import { UserModule } from '@shtifh/user-service';
+import { FcmModule } from '@shtifh/fcm-service';
 
 @Module({
-  imports: [PrismaModule, UserModule],
+  imports: [PrismaModule, UserModule, FcmModule],
   controllers: [AuthResourceController],
   providers: [AuthResourceService, AuthResourceController],
   exports: [AuthResourceService, AuthResourceController],
