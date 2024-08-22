abec0bdd6a78800679367ade4043c98918f22a8c
AbdElrahman Shaban <3bdoofficial0@gmail.com>
2024-08-22 09:00:06 +0300

    test deploy

--- apps/shtifh/admin/server/src/app/app.module.ts
+++ apps/shtifh/admin/server/src/app/app.module.ts
@@ -4,17 +4,13 @@ import {
   ValidationPipe,
 } from '@nestjs/common';
 
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { AdminShtifhModule } from '@shtifh/admin';
 import { EnvModule } from '@shtifh/env-service';
 import { APP_INTERCEPTOR, APP_PIPE } from '@nestjs/core';
 
 @Module({
   imports: [AdminShtifhModule, EnvModule.forRoot(process.env)],
-  controllers: [AppController],
   providers: [
-    AppService,
     {
       provide: APP_INTERCEPTOR,
       useClass: ClassSerializerInterceptor,
